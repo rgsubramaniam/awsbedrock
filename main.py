d602129fca2c1cfb0f8129f4cf1e6ad4a40b58ee
Ganapathy subramaniam <rgsubramaniam@gmail.com>
2024-06-15 22:25:19 +0100

    updated

--- main.py
+++ main.py
@@ -10,17 +10,79 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import FAISS
 from langchain.prompts import PromptTemplate
 from langchain.chains import RetrievalQA
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
 bedrock = boto3.client(service_name = "bedrock-runtime",region_name = "us-east-1")
 bedrock_embeding = BedrockEmbeddings(model_id="amazon.titan-embed-text-v2:0",client = bedrock)
+
+prompt_template = """
+
+Human: Use the following pieces of context to provide a 
+concise answer to the question at the end but use atleast summarize with 
+250 words with detailed explantions. If you don't know the answer, 
+just say that you don't know, don't try to make up an answer.
+<context>
+{context}
+</context
+
+Question: {question}
+
+Assistant:"""
 
 def get_documents():
     loader = PyPDFDirectoryLoader("data")
     documents = loader.load()
     text_split = RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=100)
     docs = text_split.split_documents(documents)
+    return docs
 
+def get_vectorstore(docs):
+    vectorstore_faiss =  FAISS.from_documents(
+    docs,
+    bedrock_embeding
+    )
+    vectorstore_faiss.save_local("faiss_local")
 
+def get_llm():
+    llm = Bedrock(model_id = "mistral.mistral-7b-instruct-v0:2", client = bedrock)
+    return llm
     
+PROMPT = PromptTemplate(
+    template=prompt_template, input_variables=["context", "question"]
+)
 
+def getllm_response(llm, vectorstore_faiss, query):
+    qa = RetrievalQA.from_chain_type(
+        llm = llm,
+        chain_type = "stuff",
+        retriever= vectorstore_faiss.as_retriever(
+        search_type="similarity", search_kwargs={"k": 3}),
+        return_source_documents = True,
+        chain_type_kwargs={"prompt": PROMPT})
+
+    response = qa({"query": query})
+    return response['result']
+
+def main():
+    st.set_page_config("RAG")
+    st.header("End to end RAG using Bedrock")
+
+    user_question = st.text_input("Ask a question from the PDF file")
+
+    with st.sidebar:
+        st.title("Update & create vectore store")
+
+        if st.button("Store Vector"):
+            with st.spinner("Processing.."):
+                docs = get_documents()
+                get_vectorstore(docs)
+                st.success("Done")
+
+        if st.button("Send"):
+            with st.spinner("Processing.."):
+               faiss_index = FAISS.load_local("faiss_local", bedrock_embeding, allow_dangerous_deserialization=True) 
+               llm = get_llm()
+               st.write(getllm_response(llm,faiss_index,  user_question))
+
+if __name__ == "__main__":
+    main()
